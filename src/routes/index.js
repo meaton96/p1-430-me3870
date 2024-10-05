cd92036992c5d831a34184f220b4bca653bd2748
meaton96 <lammara96@gmail.com>
2024-10-05 17:43:42 -0400

    super basic home page for check point 1

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,15 @@ const router = express.Router();
 const path = require('path');
 
 const filePath404Page = path.resolve(__dirname, '../../client/404.html');
+const simpleHomePage = path.resolve(__dirname, '../../client/index.html');
 
-// router.get('/', (req, res) => {
-//   res.send('Hello world!');
-// });
+router.get('/', (req, res) => {
+  if (simpleHomePage) {
+    res.status(200).sendFile(simpleHomePage);
+  } else {
+    res.status(404).sendFile(filePath404Page);
+  }
+});
 // router.get('/bye', (req, res) => {
 //   res.send('Goodbye!');
 // });
